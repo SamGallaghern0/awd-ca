eded26ab7116a18abcefe6aa484c67db4c1cb0cc
samgallagher <samagall2005@gmail.com>
2025-10-13 14:46:44 +0100

    13/10/2025

--- 2025-Game-Score-App/resources/views/games/show.blade.php
+++ 2025-Game-Score-App/resources/views/games/show.blade.php
@@ -6,5 +6,19 @@
     </x-slot>
 
     <div class="py-12">
-        
+        <div class="max-w-7x1 mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    <h3 class="font-semibold text-lg mb-4">Game Details</h3>
+                        <x-game-details
+                            :title="$game->title"
+                            :image="$game->image"
+                            :year="$game->year"
+                            :genre="$game->genre"
+                            :description="$game->description"
+                        />
+                </div>
+            </div>
+        </div>
+    </div>
 </x-app-layout>
